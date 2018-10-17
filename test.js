dd170fc836a2c137dcae2c1b51a24f09a6d60758
tinawuc <44070399+tinawuc@users.noreply.github.com>
2018-10-17 19:08:08 -0400

    Update test.js
    Introduce error

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 /*************Test******************/
 var test = require('unit.js');
-var str = 'Hello, world!';
+// var str = 'Hello, world!';
+var str = 'Hi, world:';
 
 test.string(str).startsWith('Hello');
 
